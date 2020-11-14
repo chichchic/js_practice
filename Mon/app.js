ff97f0b5559a75f2c72144c176082ed372e4e58e
chicichich <pch6789@naver.com>
2020-11-14 17:01:04 +0900

    WebGL-2

--- Mon/app.js
+++ Mon/app.js
@@ -37,13 +37,16 @@ function createProgram(gl, vertexShader, fragmentShader) {
 var program = createProgram(gl, vertexShader, fragmentShader);
 
 var positionBuffer = gl.createBuffer();
+/*
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-var positions = [0, 0, 0, 0.5, 0.7, 0];
+var positions = [10, 20, 200, 20, 10, 150, 10, 150, 200, 20, 200, 150];
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+*/
 
 var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-
+var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+var colorUniformLocation = gl.getUniformLocation(program, "u_color");
 gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
 // canvas를 clear처리
@@ -52,7 +55,11 @@ gl.clear(gl.COLOR_BUFFER_BIT);
 
 // 어느 셰이더 프로그램을 사용할지 지정
 gl.useProgram(program);
+// 해상도 설정
+gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 gl.enableVertexAttribArray(positionAttributeLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
 var size = 2; // 각 반복마다 2개씩 버퍼 데이타 참조
 var type = gl.FLOAT; // 32bit 부동 소수점 값
 var normalize = false; // 데이터를 노말라이즈 하지 않는다.
@@ -67,8 +74,58 @@ gl.vertexAttribPointer(
   offset
 );
 
+/*
 //GLSL 프로그램을 실행하도록 요청
 var primitiveType = gl.TRIANGLES;
 var offset = 0;
-var count = 3;
+var count = 6;
 gl.drawArrays(primitiveType, offset, count);
+*/
+
+for (var i = 0; i < 50; ++i) {
+  // 무작위 사각형을 설정합니다.
+  // 이것은 positionBuffer에 설정할 겁니다.
+  // 왜냐하면 위 코드에서 마지막
+  // ARRAY_BUFFER 바인드 포인트는
+  // positionBuffer이기 때문입니다.
+  setRectangle(
+    gl,
+    randomInt(300), //x
+    randomInt(300), //y
+    randomInt(300), //width
+    randomInt(300) //height
+  );
+
+  // 무작위 색을 셋팅한다.
+  gl.uniform4f(
+    colorUniformLocation,
+    Math.random(), //red
+    Math.random(), //green
+    Math.random(), //blue
+    1 //opacity
+  );
+
+  // 사각형을 그린다.
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
+}
+
+function randomInt(range) {
+  return Math.floor(Math.random() * range);
+}
+
+// 사각형을 정의하는 값으로 버퍼에 제공함
+function setRectangle(gl, x, y, width, height) {
+  var x1 = x;
+  var x2 = x + width;
+  var y1 = y;
+  var y2 = y + height;
+  //참고 : gl.bufferData(gl.ARRAY_BUFFER, ...) 는 현재
+  //`ARRAY_BUFFER` 바인드 포인트가 가리키는 버퍼에만 영향을 줍니다.
+  //만약 여러개의 버퍼를 가지고 있다면
+  //그 버퍼를 먼저 바인딩 해야 합니다.
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]),
+    gl.STATIC_DRAW
+  );
+}
